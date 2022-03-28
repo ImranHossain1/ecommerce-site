e80e945e6f4524593b00ea1f41cd57c2d009a0dc
ImranHossain1 <imranhossain1402@gmail.com>
2022-03-28 19:39:03 +0200

    add react router dom and hooks

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './componants/Header/Header';
+import Inventory from './componants/Inventory/Inventory';
+import NotFound from './componants/NotFound/NotFound';
+import OrderReview from './componants/OrderReview/OrderReview';
 import Shop from './componants/Shop/Shop';
+
 
 
 function App() {
   return (
     <div className="App">
+      <Router>
       <Header></Header>
-      <Shop></Shop>
+        <Routes>
+          <Route path="/"></Route>
+          <Route path="/shop" element={<Shop/>}></Route>
+          <Route path="/review" element={<OrderReview/>}></Route>
+          <Route path="/inventory" element={<Inventory/>}></Route>
+          <Route path="*" element={<NotFound/>}></Route>
+        </Routes>
+      </Router>
+      
     </div>
   );
 }
--- src/componants/Cart/Cart.js
+++ src/componants/Cart/Cart.js
@@ -2,25 +2,31 @@ import React from 'react';
 import './Cart.css';
 const Cart = (props) => {
     const {cart} = props;
-    /*/let total = 0;
+    let total = 0;
+    let totalQuantity = 0
     for (const product of cart){
-        total =total + product.price;
-    }*/
-    const totalReducer= (previous,product)=> previous+ product.price;
-    const total = cart.reduce(totalReducer, 0);
+        if(!product.quantity){
+            product.quantity = 1;
+        }
+        total =total + product.price* product.quantity;
+        totalQuantity = totalQuantity + product.quantity;
+    }
 
-    const shipping = 15;
-    const tax= (total+shipping) *10;
+    // const totalReducer= (previous,product)=> previous+ product.price;
+    // const total = cart.reduce(totalReducer, 0);
+
+    const shipping = total>0? 15 : 0;
+    const tax= (total+shipping) *0.10;
     const grandTotal = total+ tax+ shipping;
     return (
         <div>
             <h3>Order Summary</h3>
-            <h5>Items Order: {cart.length} </h5>
+            <h5>Items Order: {totalQuantity} </h5>
             <br />
             <p>Total: {total.toFixed(2)}</p>
             <p>Shipping: {shipping}</p>
-            <p>tax: {tax}</p>
-            <p>Grand Total: {grandTotal}</p>
+            <p>tax: {tax.toFixed(2)}</p>
+            <p>Grand Total: {grandTotal.toFixed(2)}</p>
         </div>
     );
 };
